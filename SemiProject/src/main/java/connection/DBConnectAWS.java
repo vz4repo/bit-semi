ca8b72edc48d56fdb2540574a8ccbc0145935cdb
sanguni <psnaga4@naver.com>
2021-10-11 18:13:05 +0900

    +paging

---
+++ SemiProject/src/main/java/connection/DBConnectAWS.java
@@ -0,0 +1,120 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DBConnectAWS {
+  // ORA_CLOUD
+  private static final String ORA_DRIVER = "oracle.jdbc.driver.OracleDriver";
+  private static final String ORA_URL =
+      "jdbc:oracle:thin:@oradb_high?TNS_ADMIN=D:/java/Wallet_oradb";
+  // "jdbc:oracle:thin:@oradb_high?TNS_ADMIN=/Users/rk/Documents/java/Wallet_oradb";
+  private static final String ORA_USER = "admin";
+  private static final String ORA_PWD = "1234qwer";
+
+  // MARIA_BIT
+  private static final String MARIA_DRIVER = "org.mariadb.jdbc.Driver";
+  private static final String MARIA_URL =
+      "jdbc:mariadb://maria-bit.cy2ifmxzl5mi.ap-northeast-2.rds.amazonaws.com:3306";
+  private static final String MARIA_USER = "guest";
+  private static final String MARIA_PWD = "1234qwer";
+
+  // DBConnect test
+  public static void main(String[] args) {
+    DBConnectAWS conn = new DBConnectAWS();
+    conn.getConnectionCloud();
+  }
+
+  public DBConnectAWS() {
+    // if (ORA_DRIVER != null) {
+    // try {
+    // Class.forName(ORA_DRIVER);
+    // System.out.println("Driver load success >>" + Thread.currentThread().getName());
+    // } catch (ClassNotFoundException e) {
+    // System.out.println("Driver load failed ");
+    // e.getLocalizedMessage();
+    // }
+
+    if (MARIA_DRIVER != null) {
+      try {
+        Class.forName(MARIA_DRIVER);
+        System.out.println("MARIA Driver load >>" + Thread.currentThread().getName());
+      } catch (ClassNotFoundException e) {
+        System.out.println("Driver load failed " + e.getLocalizedMessage());
+      }
+
+      // driver 값, name 확인
+      // Enumeration<Driver> driverList = DriverManager.getDrivers();
+      // while (driverList.hasMoreElements()) {
+      // Driver driver = (Driver) driverList.nextElement();
+      // System.out.println(driver.getClass().getName());
+      // }
+    }
+  }
+
+
+  public Connection getConnectionCloud() {
+    Connection conn = null;
+    try {
+      // conn = DriverManager.getConnection(ORA_URL, ORA_USER, ORA_PWD);
+      conn = DriverManager.getConnection(MARIA_URL, MARIA_USER, MARIA_PWD);
+      System.out.println("AWS:mariaDB getConnection");
+
+    } catch (SQLException e) {
+      System.out.println("connection error" + e.getLocalizedMessage());
+    }
+    return conn;
+
+  }
+
+
+  // 3개 close, 각각+디폴트>오버로딩 4개
+  public void resourceClose(ResultSet rs, Statement stmt, Connection conn) {
+    try {
+      if (rs != null)
+        rs.close();
+      if (stmt != null)
+        stmt.close();
+      if (conn != null)
+        conn.close();
+    } catch (Exception e) {
+    }
+  }
+
+  public void resourceClose(ResultSet rs, PreparedStatement ps, Connection conn) {
+    try {
+      if (rs != null)
+        rs.close();
+      if (ps != null)
+        ps.close();
+      if (conn != null)
+        conn.close();
+    } catch (Exception e) {
+    }
+  }
+
+  public void resourceClose(Statement ps, Connection conn) {
+    try {
+      if (ps != null)
+        ps.close();
+      if (conn != null)
+        conn.close();
+    } catch (Exception e) {
+    }
+  }
+
+  public void resourceClose(PreparedStatement ps, Connection conn) {
+    try {
+      if (ps != null)
+        ps.close();
+      if (conn != null)
+        conn.close();
+    } catch (Exception e) {
+    }
+  }
+
+}
---
+++ SemiProject/src/main/webapp/index.jsp
@@ -0,0 +1,27 @@
+<%@ page language="java" contentType="text/html; charset=utf-8"
+    pageEncoding="utf-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>세미프로젝트</title>
+<link rel="stylesheet" href="css/style.css">
+<script src="https://code.jquery.com/jquery-3.5.0.js"></script>
+</head>
+<%
+	String mainpage="layout/main.jsp";
+	if(request.getParameter("main")!=null){
+		mainpage=request.getParameter("main");
+}
+%>
+<body>
+	<!-- 헤더 (로고,메뉴들까지 불러옴) -->
+	<div>
+		<jsp:include page="layout/header.jsp"/>
+	</div>
+	<!-- 메인 레이아웃 불러옴 -->
+	<div class="layout main">
+		<jsp:include page="<%=mainpage %>"/>
+	</div>
+</body>
+</html>
